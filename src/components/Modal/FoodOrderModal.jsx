0c43bc91974f902bb4f1a1bc8342fe9242b15d96
Çağrı Dai <114882717+cagridai@users.noreply.github.com>
2023-11-28 20:36:36 +0300

    düzeldi

--- src/components/Modal/FoodOrderModal.jsx
+++ src/components/Modal/FoodOrderModal.jsx
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { changeState } from "@/redux/slices/foodOrderSlice";
 import { useEffect, useRef } from "react";
 import Address from "@/components/Layout/Sidebar/RightSideBar/Address";
@@ -8,18 +8,16 @@ import Profile from "@/components/Layout/Sidebar/RightSideBar/Profile";
 import styles from "./FoodOrderModal.module.css";
 export default function FoodOrderModal() {
   const dispatch = useDispatch();
-  let foodOrderRef = useRef();
+  const foodOrderState = useSelector((state) => state.foodOrder);
+  let foodOrderRef = useRef({});
 
   useEffect(() => {
     const handler = (e) => {
-      /*if (!foodOrderRef.current(e.target)) {
+      if (foodOrderRef.current && !foodOrderRef.current.contains(e.target)) {
         dispatch(changeState(!foodOrderState.modalState));
-      }*/
-      console.log(foodOrderRef.current.contains("mahmut"));
-
-      /*console.log(e.target);*/
+      }
     };
-    document.addEventListener("click", handler);
+    document.addEventListener("mousedown", handler);
   }, []);
 
   return (
